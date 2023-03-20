16428381ee53fc26b705d425e858782d47404ded
proloypaul <proloyp23@gmail.com>
2023-03-20 21:53:29 +0600

    style header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Header.css";
-
+import {BsCartDash} from 'react-icons/bs'; 
+import navLogo from "../../Images/logo.png";
 const Header = () => {
     return (
         <div className='headerContainer'>
             <nav className='navbar'>
                 <div className='navbarLogo'>
-                    <h1>logo place</h1>
+                    <img src={navLogo} alt='Empty!'/>
                 </div>
                 <div className='navbarOption'>
                     <ul>
-                        <li><Link to="/cart">cart</Link></li>
+                        <li><Link to="/cart"><BsCartDash/></Link></li>
                         <li><Link to="/signIn" className='commonButton'>SignIn</Link></li>
                         <li><Link to="/signUp">SingUp</Link></li>
                     </ul>
