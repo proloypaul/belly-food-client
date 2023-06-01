ad1b60a8331bf326e032122acb67d2e316036e50
proloypaul <proloyp23@gmail.com>
2023-06-02 00:11:40 +0600

    control button according to process

--- src/Components/Carts/Carts.js
+++ src/Components/Carts/Carts.js
@@ -5,10 +5,13 @@ import "./Carts.css";
 import Usefirebase from "../../Hooks/Usefirebase";
 import { Link } from "react-router-dom";
 import { handleDltCart } from "../../CommonStyle/CommonCode";
+import Swal from "sweetalert2";
 
 const Carts = () => {
   const { user } = Usefirebase();
   const [cartsData, setCartsData] = useState([]);
+  const [saveData, setSaveData] = useState({});
+
   // console.log(user?.email);
   //fetch carts data according to user email
   useEffect(() => {
@@ -16,7 +19,7 @@ const Carts = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setCartsData(data);
       });
   }, [user.email]);
@@ -34,25 +37,84 @@ const Carts = () => {
   // get tax according to subtotal price
   const tax = 5 / 100;
   const taxAmount = parseFloat((tax * subtotal).toFixed(2));
-
   // total price of user order
   const totalPriceWithTax = taxAmount + subtotal + 1; // one is delivery charge of user order
 
+  // collect save and continue data
+  const collectSaveData = (event) => {
+    const value = event.target.value;
+    const field = event.target.name;
+    const allSaveData = { ...saveData };
+    allSaveData[field] = value;
+    setSaveData(allSaveData);
+    // console.log(field, value);
+  };
+  // handle save and continue button
+  const handleSaveAndContinueData = (event) => {
+    event.preventDefault();
+    console.log(saveData);
+  };
+
+  const handlePlaceOrder = () => {
+    if (saveData.YourLocation) {
+      console.log("You are ready to order Now");
+    } else {
+      Swal.fire(
+        "Forgate SomeThing...?",
+        "Do not forgate to Save & Continue YourLocation?",
+        "question"
+      );
+    }
+  };
   return (
     <div className="cartsContainer">
       <div className="editDelivery">
-        <div>
+        <form onSubmit={handleSaveAndContinueData}>
           <h1 className="editDeliveryTitle">
             <p className="title">Edit Delibery Details</p>
           </h1>
           {/* <span className='editbottom'></span> */}
-          <input type="text" placeholder="Delivery to ..." />
-          <input type="text" placeholder="Your location" />
-          <input type="text" placeholder="Flat, suite or floor" />
-          <input type="text" placeholder="Bussiness Name" />
-          <input type="text" placeholder="Add deliver Instructor" />
-          <button type="submit">Save & continue</button>
-        </div>
+          <input
+            type="text"
+            name="DeliveryTo"
+            placeholder="Delivery to ..."
+            onBlur={collectSaveData}
+          />
+          <input
+            type="text"
+            name="YourLocation"
+            placeholder="Your location"
+            onBlur={collectSaveData}
+            required
+          />
+          <input
+            type="text"
+            name="Flat-Suite-floor"
+            placeholder="Flat, suite or floor"
+            onBlur={collectSaveData}
+          />
+          <input
+            type="text"
+            name="Bussiness Name"
+            placeholder="Bussiness Name"
+            onBlur={collectSaveData}
+          />
+          <input
+            type="text"
+            name="deliverInstructor"
+            placeholder="Add deliver Instructor"
+            onBlur={collectSaveData}
+          />
+          {saveData.YourLocation ? (
+            <button type="submit" className="saveAndContinueBtnSaved" disabled>
+              Continue to Place Order
+            </button>
+          ) : (
+            <button type="submit" className="saveAndContinueBtnUnsaved">
+              Save & Continue
+            </button>
+          )}
+        </form>
       </div>
       <div className="allCarts">
         <div className="carts">
@@ -63,7 +125,8 @@ const Carts = () => {
             Arriving in <span className="cartTitleOne">20-30 min</span>
           </p>
           <p className="cartTitleThree">
-            Your location<span className="cartTitleOne">....</span>
+            Your location{" "}
+            <span className="cartTitleOne">{saveData.YourLocation}</span>
           </p>
           <div>
             {cartsData.length !== 0 ? (
@@ -125,7 +188,9 @@ const Carts = () => {
               <p>$ {totalPriceWithTax}</p>
             </div>
           </div>
-          <button className="checkoutFoodBtn">Place Order</button>
+          <button className="checkoutFoodBtn" onClick={handlePlaceOrder}>
+            Place Order
+          </button>
         </div>
       </div>
     </div>
